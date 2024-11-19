8ba494051308faa6c142936350e4d51a12e15693
connectsaravanakumar <connectsaravanakumar@gmail.com>
2024-11-19 17:29:48 +0530

    Add project files.

---
+++ Csharp Learnings/Class/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Csharp_Learnings.Class
+{
+    public static class StringExtensions
+    {
+        public static bool checkNullOrEmpty([NotNullWhen(false)] this string value)
+        {
+
+            return value == null || value.Length == 0;
+        }
+
+    }
+}
---
+++ Csharp Learnings/Controllers/ExtensionMethodsController.cs
@@ -0,0 +1,19 @@
+﻿using Csharp_Learnings.Class;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Csharp_Learnings.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExtensionMethodsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ExtensionMethodCheckNullOrEmpty(string? valueToCheck =null)
+        {
+            bool result =  valueToCheck.checkNullOrEmpty();
+            return Ok(result);
+
+        }
+    }
+}
